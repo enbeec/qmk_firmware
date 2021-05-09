d2150d3a55a7b3aabbe779bc96da8a56e9b30775
Val Currie <contact@valcuri.email>
2021-05-09 12:14:57 -0500

    new planck-y layout

--- keyboards/idobo/keymaps/enbeec/keymap.c
+++ keyboards/idobo/keymaps/enbeec/keymap.c
@@ -1,51 +1,83 @@
 #include QMK_KEYBOARD_H
+#include "keymap.h"
 
 // idobo 5x15 ortholinear keyboard
 // Val's Keymap Version 01
 
-
 enum {
-	_QWERTY = 0,
-	_FN,
+	QWERTY = 0,
+    _RAISE,
+    _LOWER,
+	FN_LAY,
 	_BONUS,
-	_ADJUST
+	ADJUST
 };
 
+// ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
+// │ ! │ @ │ # │ $ │ % │ ^ │ & │ * │ ( │ ) │
+// │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │
+// │ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 0 │
+// └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
+// ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
+// │   │   │   │   │   │ | │ _ │ + │ { │ } │
+// │ A │ S │ D │ F │ G │ H │ J │ K │ L │ ; │
+// │   │   │   │   │   │ \ │ - │ = │ [ │ ] │
+// └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
+// ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
+// │   │   │   │   │   │   │ ~ │   │   │   │
+// │ Z │ X │ C │ V │ B │ N │ M │ , │ . │ / │
+// │   │   │   │   │   │   │ ` │   │   │   │
+// └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[_QWERTY] = LAYOUT_ortho_5x15(
-			KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, TT(_BONUS), KC_DEL, 
-			KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP, 
-			LT(_ADJUST,KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_INS, KC_PGDN, 
-			KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_HOME, KC_UP, KC_END, 
-			TT(_FN), KC_LCTL, KC_LGUI, KC_LALT, KC_BSPC, LT(_FN,KC_SPC), LT(_FN,KC_SPC), KC_ENT, KC_RALT, KC_RGUI, KC_RCTL, HYPR(KC_APP), KC_LEFT, KC_DOWN, KC_RGHT
+	[QWERTY] = LAYOUT_ortho_5x15(
+            KC_GRAVE,   NUM__ROW___L,   NUM__ROW___R,   _______,_______,_______,_______,
+            KC_TAB,     Q___THRU___T,   Y___THRU___P,   _______,_______,_______,_______,
+            KC_ESC,     A___THRU___F,   H_THRU_COLON,   _______,_______,_______,_______,
+            KC_LSFT,    Z___THRU___B,   N_THRU_SLASH,   _______,_______,_______,_______,
+            FN_TAP,     TRI_L,  PLANCK_SPACE,  TRI_R,   _______,_______,_______,_______
 			),
-	[_FN] = LAYOUT_ortho_5x15(
-			KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_TRNS, 
-			KC_TRNS, KC_TRNS, KC_MS_BTN1, KC_UP,  KC_MS_BTN2, KC_TRNS, KC_TRNS, KC_PMNS, KC_P7, KC_P8, KC_P9, KC_PPLS, KC_TRNS, KC_TRNS, KC_TRNS, 
-			KC_CAPS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_PAST, KC_P4, KC_P5, KC_P6, KC_PCMM, KC_TRNS, KC_TRNS, KC_TRNS, 
-			KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSLS, KC_P1, KC_P2, KC_P3, KC_PEQL, KC_TRNS, KC_TRNS, KC_TRNS, 
-			KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P0, KC_PDOT, KC_ENT, KC_TRNS, KC_TRNS, KC_TRNS
+	[_LOWER] = LAYOUT_ortho_5x15(
+			_______,____________FUNC_KEY__L________________,____________FUNC_KEY__R________________,____________4_TRANS____________,
+            _______,____________LOWER__R1_L________________,____________LOWER__R1_R________________,____________4_TRANS____________,
+            _______,____________LOWER__R2_L________________,____________LOWER__R2_R________________,____________4_TRANS____________,
+            _______,____________LOWER__R3_L________________,____________LOWER__R3_R________________,____________4_TRANS____________,
+			_______,_______________5_TRANS_________________,_______________5_TRANS_________________,____________4_TRANS____________
 			),
-	
+	[_RAISE] = LAYOUT_ortho_5x15(
+			_______,____________FUNC_KEY__L________________,____________FUNC_KEY__R________________,____________4_TRANS____________,
+            _______,____________RAISE__R1_L________________,____________RAISE__R1_R________________,____________4_TRANS____________,
+            _______,____________RAISE__R2_L________________,____________RAISE__R2_R________________,____________4_TRANS____________,
+            _______,____________RAISE__R3_L________________,____________RAISE__R3_R________________,____________4_TRANS____________,
+			_______,_______________5_TRANS_________________,_______________5_TRANS_________________,____________4_TRANS____________
+			),
+	[FN_LAY] = LAYOUT_ortho_5x15(
+			_______,____________FUNC_KEY__L________________,____________FUNC_KEY__R________________,____________4_TRANS____________,
+			_______,_______,_______, KC_UP, _______,_______,_______,_______,_______,_______,_______,____________4_TRANS____________,
+			KC_CAPS,_______,KC_LEFT,KC_DOWN,KC_RGHT,_______,KC_LEFT,KC_DOWN, KC_UP ,KC_RGHT,_______,____________4_TRANS____________,
+			_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,____________4_TRANS____________,
+			_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,____________4_TRANS____________
+			),
+
 	[_BONUS] = LAYOUT_ortho_5x15(
-			KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-			KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_WH_DOWN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-			KC_TRNS, KC_TRNS, KC_MS_WH_RIGHT, KC_MS_WH_UP, KC_MS_WH_LEFT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-			KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-			KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+            _______,_______________5_TRANS_________________,_______________5_TRANS_________________,____________4_TRANS____________,
+            _______,_______________5_TRANS_________________,_______________5_TRANS_________________,____________4_TRANS____________,
+            _______,_______________5_TRANS_________________,_______________5_TRANS_________________,____________4_TRANS____________,
+            _______,_______________5_TRANS_________________,_______________5_TRANS_________________,____________4_TRANS____________,
+            _______,_______________5_TRANS_________________,_______________5_TRANS_________________,____________4_TRANS____________
 			),
-	
-	[_ADJUST] = LAYOUT_ortho_5x15(
-			KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-			KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-			KC_TRNS, KC_TRNS, KC_TRNS, DEBUG, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_TOG, KC_TRNS, 
-			KC_TRNS, KC_Z, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, AG_SWAP, KC_TRNS, AG_NORM, 
-			KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+
+	[ADJUST] = LAYOUT_ortho_5x15(
+			_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+			_______,_______,_______,_______, RESET ,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+			_______,_______,_______, DEBUG ,_______,_______,_______,_______,_______,_______,_______,_______,_______,RGB_TOG,_______,
+            _______,_______________5_TRANS_________________,_______________5_TRANS_________________,____________4_TRANS____________,
+            _______,_______,AG_NORM,AG_SWAP,_______,_______,_______________5_TRANS_________________,____________4_TRANS____________
 			)
 
 };
 
-uint8_t prev = _QWERTY;
+uint8_t prev = QWERTY;
 uint32_t check;
 uint32_t desired = 9;
 
@@ -56,18 +88,18 @@ void matrix_init_user() {
 uint32_t layer_state_set_user(uint32_t state) {
 	uint8_t layer = biton32(state);
 	/* no color change for adjustments layer */
-	if (prev != _ADJUST) {
+	if (prev != ADJUST) {
 		switch (layer) {
-			case _QWERTY:
+			case QWERTY:
 				rgblight_sethsv(HSV_PURPLE);
 				break;
-			case _FN:
+			case FN_LAY:
 				rgblight_sethsv(HSV_WHITE);
 				break;
 			case _BONUS:
 				rgblight_sethsv(HSV_ORANGE);
 				break;
-			case _ADJUST:
+			case ADJUST:
 				break;
 		}
 	} else {
